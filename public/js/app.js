a351f214170feadd7728591742e2d720b84e4c75
Saheed Ogunsolu <saheedogunsolu2016@gmail.com>
2021-05-10 23:21:04 +0100

    setting up config for heroku

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+const weatherForm = document.querySelector("form");
+const search = document.querySelector(".input");
+const message1 = document.querySelector("#message-1");
+const message2 = document.querySelector("#message-2");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = search.value;
+  message1.textContent = "loading..........";
+  message2.textContent = "";
+
+  const url = `/weather?address=${location}`;
+  fetch(url).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        message1.textContent = data.error;
+        message2.textContent = "";
+      } else {
+        message1.textContent = data.location;
+        message2.textContent = data.forecast;
+      }
+    });
+  });
+});
